9741c92cb29f6ac5cc44cf9a57403d606a889f86
42erenbey4242-hue <42erenbey4242@gmail.com>
2026-03-15 17:29:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// Kullanıcıyı Başlat
+let kullanici = JSON.parse(localStorage.getItem("user_data")) || {
+    isim: "BlockGuy" + Math.floor(Math.random()*9999),
+    tix: 50, // Başlangıç parası
+    envanter: ["bacon_skin"], // Başlangıç kostümü
+    giyilen: "bacon_skin"
+};
 
+function tixHarca(esyaId) {
+    let esya = marketEsyalari.find(e => e.id === esyaId);
+    if (kullanici.tix >= esya.fiyat) {
+        kullanici.tix -= esya.fiyat;
+        kullanici.envanter.push(esya.isim);
+        localStorage.setItem("user_data", JSON.stringify(kullanici));
+        alert("Satın alındı!");
+    } else {
+        alert("Yetersiz BlockTix!");
+    }
+}
