c6df0fa3db383f806a93dbb3e5a95ee23b56d1ee
42erenbey4242-hue <42erenbey4242@gmail.com>
2026-03-15 17:29:31 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,5 @@
-
+const marketEsyalari = [
+    { id: 1, isim: "Klasik Şapka", fiyat: 25, tip: "sapka", resim: "sapka1.png" },
+    { id: 2, isim: "Havalı Gözlük", fiyat: 15, tip: "gozluk", resim: "gozluk1.png" },
+    // ... buraya 50 taneye kadar ekle
+];
