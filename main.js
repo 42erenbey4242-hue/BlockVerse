b9ede353745545f594a4c189c9e11a2b96a3823a
42erenbey4242-hue <42erenbey4242@gmail.com>
2026-03-15 17:30:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-// Kullanıcıyı Başlat
-let kullanici = JSON.parse(localStorage.getItem("user_data")) || {
-    isim: "BlockGuy" + Math.floor(Math.random()*9999),
-    tix: 50, // Başlangıç parası
-    envanter: ["bacon_skin"], // Başlangıç kostümü
-    giyilen: "bacon_skin"
+// Başlangıç Ayarları
+let user = JSON.parse(localStorage.getItem("user_data")) || {
+    name: "BlockGuy" + Math.floor(Math.random() * 9999),
+    tix: 50,
+    inventory: []
 };
 
-function tixHarca(esyaId) {
-    let esya = marketEsyalari.find(e => e.id === esyaId);
-    if (kullanici.tix >= esya.fiyat) {
-        kullanici.tix -= esya.fiyat;
-        kullanici.envanter.push(esya.isim);
-        localStorage.setItem("user_data", JSON.stringify(kullanici));
-        alert("Satın alındı!");
+// Sayfa yüklendiğinde çalış
+window.onload = function() {
+    if(document.getElementById("user-name")) {
+        document.getElementById("user-name").innerText = user.name;
+        document.getElementById("tix-balance").innerText = user.tix;
+    }
+};
+
+// Satın alma fonksiyonu
+function buyItem(itemName, price) {
+    if(user.tix >= price) {
+        user.tix -= price;
+        user.inventory.push(itemName);
+        localStorage.setItem("user_data", JSON.stringify(user));
+        alert(itemName + " alındı!");
+        location.reload();
     } else {
         alert("Yetersiz BlockTix!");
     }
